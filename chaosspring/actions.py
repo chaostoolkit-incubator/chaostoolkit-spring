21dfcfe6689d6ed5dd1a654b30a7a842570b702d
Russ Miles <russ@russmiles.com>
2018-06-04 19:01:15 +0100

    Added setting of assaults and accompanying tests
    Signed-off-by: Russ Miles <russ@russmiles.com>

--- chaosspring/actions.py
+++ chaosspring/actions.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
+from typing import Any, Dict
+
 from chaoslib.exceptions import FailedActivity
 from chaoslib.types import Configuration, Secrets
 import requests
 
-__all__ = ["enable_chaosmonkey", "disable_chaosmonkey"]
+__all__ = ["enable_chaosmonkey",
+           "disable_chaosmonkey",
+           "change_assaults_configuration"]
 
 
 def enable_chaosmonkey(base_url: str, timeout: float = None,
                        configuration: Configuration = None,
-                       secrets: Secrets = None):
+                       secrets: Secrets = None) -> str:
     """
     Enable Chaos Monkey on a specific service
     """
@@ -27,10 +31,12 @@ def enable_chaosmonkey(base_url: str, timeout: float = None,
         raise FailedActivity(
             "Enable ChaosMonkey failed: {m}".format(m=r.text))
 
+    return r.text
+
 
 def disable_chaosmonkey(base_url: str, timeout: float = None,
                         configuration: Configuration = None,
-                        secrets: Secrets = None):
+                        secrets: Secrets = None) -> str:
     """
     Disable Chaos Monkey on a specific service
     """
@@ -48,3 +54,32 @@ def disable_chaosmonkey(base_url: str, timeout: float = None,
     if r.status_code != 200:
         raise FailedActivity(
             "Disable ChaosMonkey failed: {m}".format(m=r.text))
+
+    return r.text
+
+
+def change_assaults_configuration(base_url: str,
+                                  assaults_configuration: Dict[str, Any],
+                                  timeout: float = None,
+                                  configuration: Configuration = None,
+                                  secrets: Secrets = None) -> str:
+    """
+    Change Assaults configuration on a specific service
+    """
+
+    url = "{base_url}/chaosmonkey/assaults".format(base_url=base_url)
+
+    params = {}
+
+    if timeout is not None:
+        params["timeout"] = timeout
+
+    r = requests.post(
+        url, headers={"Accept": "application/json"}, params=params)
+
+    if r.status_code != 200:
+        raise FailedActivity(
+            "Change ChaosMonkey Assaults Configuration failed: {m}".format(
+                m=r.text))
+
+    return r.text
--- tests/test_actions.py
+++ tests/test_actions.py
@@ -4,7 +4,8 @@ import pytest
 import requests
 import requests_mock
 
-from chaosspring.actions import enable_chaosmonkey, disable_chaosmonkey
+from chaosspring.actions import enable_chaosmonkey, \
+    disable_chaosmonkey, change_assaults_configuration
 
 
 def test_enable_chaosmonkey():
@@ -16,6 +17,8 @@ def test_enable_chaosmonkey():
 
         enabled = enable_chaosmonkey(
             base_url="http://localhost:8080/actuator")
+
+    assert enabled == "Chaos Monkey is enabled"
 
 
 def test_enable_chaosmonkey_fails():
@@ -53,3 +56,51 @@ def test_disable_chaosmonkey_fails():
                 base_url="http://localhost:8080/actuator")
 
         assert "Disable ChaosMonkey failed" in str(ex)
+
+
+def test_change_assaults_configuration():
+    assaults_configuration = {
+        "level": 5,
+        "latencyRangeStart": 2000,
+        "latencyRangeEnd": 5000,
+        "latencyActive": True,
+        "exceptionsActive": True,
+        "killApplicationActive": False,
+        "restartApplicationActive": False
+    }
+
+    with requests_mock.mock() as m:
+        m.post(
+            "http://localhost:8080/actuator/chaosmonkey/assaults",
+            status_code=200,
+            text="Assault config has changed")
+
+        changed = change_assaults_configuration(
+            base_url="http://localhost:8080/actuator",
+            assaults_configuration=assaults_configuration)
+
+        assert changed == "Assault config has changed"
+
+
+def test_change_assaults_configuration_fails():
+    assaults_configuration = {
+        "level": 5,
+        "latencyRangeStart": 2000,
+        "latencyRangeEnd": 5000,
+        "latencyActive": True,
+        "exceptionsActive": True,
+        "killApplicationActive": False,
+        "restartApplicationActive": False
+    }
+
+    with requests_mock.mock() as m:
+        m.post(
+            "http://localhost:8080/actuator/chaosmonkey/assaults",
+            status_code=404)
+
+        with pytest.raises(FailedActivity) as ex:
+            change_assaults_configuration(
+                base_url="http://localhost:8080/actuator",
+                assaults_configuration=assaults_configuration)
+
+        assert "Change ChaosMonkey Assaults Configuration failed" in str(ex)
